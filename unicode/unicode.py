28b74aa495fca9b41dbf30b0e460d433363defbf
nixawk <hap.ddup@gmail.com>
2015-01-26 07:32:18 +0000

    translte string to unicode

---
+++ unicode/unicode.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+"""
+Demostration:
+
+[demo@core unicode]$ python unicode.py
+Please a string to be unicoded: - englisch
+- englisch
+[demo@core unicode]$ python unicode.py
+Please a string to be unicoded: 英语
+英语
+[demo@core unicode]$ python unicode.py
+Please a string to be unicoded: 영어
+영어
+[demo@core unicode]$ python unicode.py
+Please a string to be unicoded: اللغة الإنجليزية
+اللغة الإنجليزية
+[demo@core unicode]$ python unicode.py
+Please a string to be unicoded:  английский
+английский
+"""
+
+
+def getUnicode(value, encoding=None):
+    if isinstance(value, unicode):
+        return value
+
+    else:
+        try:
+            return unicode(value, encoding or "utf-8")
+        except (UnicodeDecodeError, TypeError):
+            return unicode(str(value), errors="ignore")
+
+
+u = raw_input('Please a string to be unicoded: ')
+print getUnicode(u)
