c3b402920a374e707cc75d874b6b4a3f21ec99e8
nixawk <hap.ddup@gmail.com>
2015-05-23 03:06:52 +0000

    scapy dns sniffer - add docstring

--- scapy/scapy-dns_sniff.py
+++ scapy/scapy-dns_sniff.py
@@ -1,6 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: utf8 -*-
 
+"""
+execte demo py with root privilege, and finish double dns query as follow.
+
+    $ nslookup search.yahoo.com
+    $ nslookup github.com
+
+dns sniffer will parse dns requests and responses automatically.
+
+    root:scapy/ #  python scapy-dns_sniff.py
+    WARNING: No route found for IPv6 destination :: (no default route?)
+
+    [*] request: 192.168.1.108:49771 -> 192.168.1.1:53 : search.yahoo.com.
+    [*] response: 192.168.1.108:49771 <- 192.168.1.1:53 : search.yahoo.com. - ds-global.l7.search.ystg1.b.yahoo.com.
+    [*] response: 192.168.1.108:49771 <- 192.168.1.1:53 : ds-global.l7.search.ystg1.b.yahoo.com. - ds-any-global.l7.search.ysta1.b.yahoo.com.
+    [*] response: 192.168.1.108:49771 <- 192.168.1.1:53 : ds-any-global.l7.search.ysta1.b.yahoo.com. - 188.125.66.104
+
+    [*] request: 192.168.1.108:40813 -> 192.168.1.1:53 : github.com.
+    [*] response: 192.168.1.108:40813 <- 192.168.1.1:53 : github.com. - 192.30.252.128
+
+"""
+
 from scapy.all import *
 
 
