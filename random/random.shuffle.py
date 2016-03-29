ed8308696a86ddccb12005a4da1e3f6b2bdc19c9
all3g <hap.ddup@gmail.com>
2016-03-29 11:17:22 +0800

    random.shuffle

---
+++ random/random.shuffle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import random
+
+x = [1, 2, 3, 4, 5]
+print("Original List: ", x)
+random.shuffle(x)
+print("Finally  List: ", x)
+
+# shuffle the order of the list
