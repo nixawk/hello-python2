a7dbf92b33e0c7e07a75e8f9e5246eb3f423fdec
Vex Woo <hap.ddup@gmail.com>
2016-06-19 03:35:52 -0500

    add Numbers

---
+++ format/6-Numbers.py
@@ -0,0 +1,12 @@
+
+"""
+Numbers
+
+Of course it is also possible to format numbers.
+"""
+
+print '%d' % (42, )
+print '{:d}'.format(42)
+
+print '%f' % (3.141592653589793, )
+print '{:f}'.format(3.141592653589793)
